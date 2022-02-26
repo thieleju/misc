00b98a541beb8d945b74f44441586eb3a29c76d3
Julian <thiele.juli@gmail.com>
2022-02-26 01:08:29 +0100

    Added vue3 and typescript tests

--- viteVue3ts/src/App.vue
+++ viteVue3ts/src/App.vue
@@ -1,13 +1,41 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div class="App">
+    <h1>{{ title }}</h1>
+
+    <button @click="increase">Count: {{ count }}</button>
+
+    <Message :message="title" @pressed="pressed" />
+    <h2>{{ message }}</h2>
+    <h2>{{ date }}</h2>
+  </div>
 </template>
+
+<script lang="ts" setup>
+import Message from "./components/Message.vue";
+import axios from "axios";
+import { ref, isRef, reactive } from "vue";
+
+let title: string = "";
+
+let message = ref("");
+let date = ref(null);
+let count = ref(1);
+
+const cube = reactive<{ length: number; width: number; height: number }>({
+  length: 10,
+  width: 20,
+  height: 33,
+});
+
+function increase(): void {
+  count.value++;
+}
+
+function pressed(data: any): void {
+  message.value = data.message;
+  date.value = data.date;
+}
+</script>
 
 <style>
 #app {
---
+++ viteVue3ts/src/components/Message.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h3 v-bind="$attrs">Hello from Message component</h3>
+    <h4>Message {{ message }}</h4>
+    <button @click="onClick">Press Me</button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  inheritAttrs: false,
+};
+</script>
+
+<script lang="ts" setup>
+// import { useAttrs } from "vue";
+// const attrs = useAttrs();
+// console.log(attrs);
+
+const emits = defineEmits(["pressed"]);
+
+function onClick(): void {
+  emits("pressed", {
+    message: "Hello from Message component",
+    date: new Date(),
+  });
+}
+
+interface props {
+  message?: string;
+}
+
+const props = withDefaults(defineProps<props>(), {
+  message: "Default message",
+});
+</script>
